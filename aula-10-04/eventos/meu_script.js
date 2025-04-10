ccc9957a09e3018e86f0842dc58f405fa6db6073
Talita G <talitatoledo3@gmail.com>
2025-04-10 19:28:17 -0300

    organizando o código

--- aula-10-04/eventos/meu_script.js
+++
@@ -1,7 +0,0 @@
-function dentro(){
-    document.getElementById("meuInput").style.background = "green";
-}
-
-function fora(){
-    document.getElementById("meuInput").style.background = "yellow";
-}
---
+++ aula-10-04/eventos/on_blur_focus.js
@@ -0,0 +1,7 @@
+function dentro(){
+    document.getElementById("meuInput").style.background = "green";
+}
+
+function fora(){
+    document.getElementById("meuInput").style.background = "yellow";
+}
