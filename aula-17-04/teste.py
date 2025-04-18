da9aaa0b38f06c5a7c83ab48f7f68f193b1d8cc1
Talita G <talitatoledo3@gmail.com>
2025-04-17 22:59:27 -0300

    clonacartao

--- aula-17-04/teste.py
+++
@@ -1,9 +0,0 @@
-
-a = float(input())
-b = float(input())
-c = float(input())
-p1 = a * 2
-p2 = b * 3
-p3 = c * 5
-media = ((p1 + p2) + p3) / 10
-print(str("MEDIA = ") + str("{:0.1f}".format(media)))
